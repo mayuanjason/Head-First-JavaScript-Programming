947d2ef89405f16d695d0c586ef45267256ebb6f
yuanm <yuanm@yuanmmatoMacBook-Air.local>
2018-09-13 19:45:30 +0800

    updating chapter1

--- chapter1/code.js
+++ chapter1/code.js
@@ -1,13 +1,13 @@
-var word = "bottles";
-var count = 99;
-while (count > 0) {
-  console.log(count + " " + word + " of beer on the wall");
-  console.log(count + " " + word + " of beer,");
-  console.log("Take one down, pass it around,");
-  count -= 1;
-  if (count > 0) {
-    console.log(count + " " + word + " of beer on the wall.");
-  } else {
-    console.log("No more " + word + "of beer on the wall.")
-  }
+var scoops = 5;
+
+while (scoops > 0) {
+    document.write("Another scoop!<br>");
+    if (scoops >= 5) {
+        alert("Eat faster, the ice cream is going to melt!");
+    } else if (scoops < 3) {
+        alert("Ice cream is running low!");
+    }
+    scoops = scoops - 1;
 }
+
+document.write("Life without ice cream isn't the same");
